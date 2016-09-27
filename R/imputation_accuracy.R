bf949284a5854da2f229e4d75ac46bb52010f9b5
Stefan <stefan.hoj-edwards@roslin.ed.ac.uk>
2016-09-27 14:09:23 +0100

    Revert "Adding profiling of memory usage. However, nothing can be concluded."
    This reverts commit 59b8c47993f1a0229dd5e78bf61a6a69062fb747.

--- R/imputation_accuracy.R
+++ R/imputation_accuracy.R
@@ -33,7 +33,7 @@
 #' }
 #' @export
 #' @seealso \code{\link{write.snps}} for writing SNPs to a file.
-imputation_accuracy <- function(truefn, imputefn, nSNPs=NULL, nAnimals=NULL, NAval=9, standardized=TRUE, fast=FALSE, full=FALSE) {
+imputation_accuracy <- function(truefn, imputefn, nSNPs=NULL, nAnimals=NULL, NAval=9, standardized=TRUE, fast=FALSE) {
   stopifnot(file.exists(truefn))
   stopifnot(file.exists(imputefn))
   
@@ -47,11 +47,7 @@ imputation_accuracy <- function(truefn, imputefn, nSNPs=NULL, nAnimals=NULL, NAv
   m <- as.integer(nSNPs)
   n <- as.integer(nAnimals)
   
-  if (full) {
-    subroutine <- ifelse(fast, 'imp_acc_fast_full','imp_acc')
-  } else {
-    subroutine <- ifelse(fast, 'imp_acc_fast','imp_acc')
-  }
+  subroutine <- ifelse(fast, 'imp_acc_fast','imp_acc')
   
   res <- .Fortran(subroutine,
                   truefn=as.character(truefn),
@@ -62,13 +58,13 @@ imputation_accuracy <- function(truefn, imputefn, nSNPs=NULL, nAnimals=NULL, NAv
                   standardized=as.integer(standardized),
                   means=vector('numeric',m), vars=vector('numeric',m),  # Placeholders for return data.
                   rowcors=vector('numeric', n), matcor=numeric(1), colcors=vector('numeric',m),
-                  rowID=vector('integer',n),
+                  rowcorID=vector('integer',m),
                   PACKAGE='Siccuracy')
   res$colcors[is.infinite(res$colcors)] <- NA
   res$colcors[is.nan(res$colcors)] <- NA
   res$rowcors[is.infinite(res$rowcors)] <- NA
   res$rowcors[is.nan(res$rowcors)] <- NA
-  res[c('means','vars','rowcors','matcor','colcors','rowID')]
+  res[c('means','vars','rowcors','matcor','colcors','rowcorID')]
 }
 
 #' Deprecated function names
