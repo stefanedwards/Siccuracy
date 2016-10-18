3161423af0f9d882ac92fc4ac0b8a7a8d3cc57ee
stefanedwards <sme@iysik.com>
2016-10-18 22:00:43 +0100

    Adding cbind_SNPs, complete with dual-format ability. #4 Fixed.

--- R/mutate_chips.R
+++ R/mutate_chips.R
@@ -8,25 +8,37 @@
 #' @param skiplines Integer, number of lines to skip before outputting. \code{nlines} counts towards total number of lines, i.e. both skipped and outputted.
 #' @param excludeids Integer vector of first column id's to \emph{exclude} from the output. \code{nlines} also counts towards excluded lines.
 #' @param format Character of Fortran formats. Defaults to \code{F5.2}; use \code{I2} for integers, or numeric() or integer() for the former two.
+#' @param int Logical, True: outputs integers only, \code{FALSE:}: also outputs decimals.
 #' @return Exit status, invisible.
 #' @export
 # subroutine rowconcatenate(files, fns, fnout, nlines, ncols, result)
-cbind_SNPs <- function(fns, fnout, nlines=NULL, ncols=NULL, skiplines=0, excludeids=integer(0), format='5.2', int=TRUE) {
+cbind_SNPs <- function(fns, fnout, nlines=NULL, skiplines=0, excludeids=integer(0), format=NULL, int=TRUE) {
   if (!all(file.exists(fns))) {
     stop('Not all files in fns exists.')
   }
   
   if (is.null(nlines)) nlines <- get_nlines(fns[1])
-  if (is.null(ncols)) ncols <- sapply(fns, get_ncols) - 1
-  if (length(ncols) < length(fns)) rep(ncols, length.out=length(fns))
+  ncols <- sapply(fns, get_ncols) - 1
   
+  if (is.null(format)) format <- ifelse(int, integer(1), numeric(1))
   if (is.integer(format)) {
-    format <- 'I2'
+    if (length(format) != 1) {
+      format <- 'I2'
+    } else if (format == 0) {
+      format <- 'I2'
+    } else {
+      format <- as.character(format)
+    }
   } else if (is.numeric(format)) {
-    format <- 'F5.2'
+    if (length(format) != 1) {
+      format <- 'F5.2'
+    } else if (format == 0.0) {
+      format <- 'F5.2'
+    } else {
+      format <- formatC(format, digits=1, format='f')
+    }
   }
-  format <- gsub('^[[:alpha:]]','',format)[1]
-  
+  if (!grepl('^[[:alpha:]]+', format)) format <- paste0(ifelse(int, 'I', 'F'), format)
   
   ftemp <- tempfile()
   write.table(fns, file = ftemp, row.names = FALSE, col.names=FALSE)
@@ -38,7 +50,7 @@ cbind_SNPs <- function(fns, fnout, nlines=NULL, ncols=NULL, skiplines=0, exclude
                   nlines=as.integer(nlines),
                   ncols=as.integer(ncols),
                   skiplines=as.integer(skiplines),
-                  idlength=length(excludeids),
+                  idlength=as.integer(length(excludeids)),
                   excludeids=as.integer(excludeids),
                   result=integer(1),
                   lenfmt=nchar(format),
--- src/mutate_chips.f95
+++ src/mutate_chips.f95
@@ -1,13 +1,16 @@
-! Cannot pass character vector from R, so we use this wrapper.
-subroutine cbindsnpsrwrapper(files, fnin, fnout, nlines, ncols, skiplines, idlength, excludeids, result, lenfmt, userfmt, int)
 
-  integer, intent(in) :: files, nlines, skiplines, idlength, lenfmt, int
+! Cannot pass character vector from R, so we use this wrapper.
+subroutine cbindsnpsrwrapper(files, fnin, fnout, nlines, ncols, skiplines, idlength, excludeids, result, lenfmt, userfmt, asint)
+  implicit none
+
+  integer, intent(in) :: files, nlines, skiplines, idlength, lenfmt, asint
   character(lenfmt), intent(in) :: userfmt
   character(255), intent(in) :: fnin, fnout
   integer, dimension(files), intent(in) :: ncols
   integer, intent(out) :: result
   integer, dimension(idlength), intent(in) :: excludeids
 
+  logical :: alsoasint
   integer :: i, stat
   character(255), dimension(files) :: fns
 
@@ -17,20 +20,19 @@ subroutine cbindsnpsrwrapper(files, fnin, fnout, nlines, ncols, skiplines, idlen
   end do
   close(64)
 
-  if (int == 1) then
-    call cbindsnpsint (files, fns, fnout, nlines, ncols, skiplines, idlength, excludeids, result, lenfmt, userfmt)
-  else
-    call cbindsnpsreal(files, fns, fnout, nlines, ncols, skiplines, idlength, excludeids, result, lenfmt, userfmt)
-  endif
+  alsoasint = asint==1
+  
+  call cbindsnps(files, fns, fnout, nlines, ncols, skiplines, idlength, excludeids, result, lenfmt, userfmt, alsoasint)
 
 end subroutine cbindsnpsrwrapper
 
 
 ! Concatenates genotype matrices (from e.g. multiple chromosomes) into one.
 ! No row ID checking.
-subroutine cbindsnpsreal(files, fns, fnout, nlines, ncols, skiplines, idlength, excludeids, result, lenfmt, userfmt)
+subroutine cbindsnps(files, fns, fnout, nlines, ncols, skiplines, idlength, excludeids, result, lenfmt, userfmt, asint)
   implicit none
 
+  logical, intent(in) :: asint
   integer, intent(in) :: files, nlines, skiplines, idlength, lenfmt
   character(lenfmt), intent(in) :: userfmt
   character(255), intent(in) :: fnout
@@ -41,29 +43,41 @@ subroutine cbindsnpsreal(files, fns, fnout, nlines, ncols, skiplines, idlength, 
 
   integer :: i, j, id, stat, k
   integer, dimension(files) :: units
-  real, dimension(:,:), allocatable :: row
+  integer, dimension(:), allocatable :: rowint
+  real, dimension(:), allocatable :: rowreal
   character(50) :: fmt0
   character(50), dimension(files) :: fmt
-  character(4), dimension(files) :: advance
+  character(4), dimension(files) :: advance, formatprefix
 
-  allocate(row(files,maxval(ncols, 1)))
+  if (asint .eqv. .true.) then
+    allocate(rowint(maxval(ncols, 1)))
+  end if
+  allocate(rowreal(maxval(ncols, 1)))
 
   advance(:) = 'no'
   advance(files) = 'yes'
-
-  do i=1,files
-    units(i) = 200 + i
-    open(units(i), file=fns(i), status='OLD')
-    write(fmt0, '(i5)') ncols(i)
-    fmt(i)='('//trim(adjustl(fmt0))//'F'//trim(adjustl(userfmt))//')'
-    !fmt(i)='('//trim(adjustl(fmt0))//'F5.2)'
-  end do
-
+  
+  if (asint .eqv. .true.) then
+    do i=1,files
+      units(i) = 200 + i
+      open(units(i), file=fns(i), status='OLD')
+      write(fmt0, '(i5)') ncols(i)
+      fmt(i)='('//trim(adjustl(fmt0))//trim(adjustl(userfmt))//')'
+    end do
+  else
+    do i=1,files
+      units(i) = 200 + i
+      open(units(i), file=fns(i), status='OLD')
+      write(fmt0, '(i5)') ncols(i)
+      fmt(i)='('//trim(adjustl(fmt0))//trim(adjustl(userfmt))//')'
+    end do
+  end if
+  
   open(55, file=fnout, status='UNKNOWN')
 
   do j=1,nlines
     fileloop: do i=1,files
-      read(units(i), *, iostat=stat) id, row(i,1:ncols(i))
+      read(units(i), *, iostat=stat) id, rowreal(1:ncols(i))
       if (stat /= 0) exit
       if (j <= skiplines) cycle
       if (idlength > 0) then
@@ -72,12 +86,20 @@ subroutine cbindsnpsreal(files, fns, fnout, nlines, ncols, skiplines, idlength, 
         enddo
       endif
       if (i == 1) write(55, '(i20)', advance='no') id
-      write(55, fmt(i), advance=advance(i)) row(i,1:ncols(i))
+      if (asint .eqv. .true.) then
+        rowint(1:ncols(i)) = rowreal(1:ncols(i))
+        write(55, fmt(i), advance=advance(i)) rowint(1:ncols(i))
+      else
+        write(55, fmt(i), advance=advance(i)) rowreal(1:ncols(i))
+      end if
     end do fileloop
     if (stat /= 0) exit
   end do
 
-  deallocate(row)
+  if (asint .eqv. .true.) then
+    deallocate(rowint)
+  end if
+  deallocate(rowreal)
 
   close(55)
   do i=1,files
@@ -86,65 +108,144 @@ subroutine cbindsnpsreal(files, fns, fnout, nlines, ncols, skiplines, idlength, 
 
   result=stat
 
-end subroutine cbindsnpsreal
+end subroutine cbindsnps
 
-! Concatenates genotype matrices (from e.g. multiple chromosomes) into one.
-! No row ID checking.
-subroutine cbindsnpsint(files, fns, fnout, nlines, ncols, skiplines, idlength, excludeids, result, lenfmt, userfmt)
-  implicit none
 
-  integer, intent(in) :: files, nlines, skiplines, idlength, lenfmt
+
+!! Merges two SNP chips
+
+subroutine combinechips(fnhd, fnld, fnout, hdcols, ldcols, outcols, &
+  nhd, hdid, nld, ldid, hdpos, ldpos,  missing, lenfmt, userfmt, status, asint)
+
+  integer, intent(in) :: hdcols, ldcols, outcols, nhd, nld, missing, lenfmt, asint
   character(lenfmt), intent(in) :: userfmt
-  character(255), intent(in) :: fnout
-  character(255), dimension(files), intent(in) :: fns
-  integer, dimension(files), intent(in) :: ncols
-  integer, dimension(idlength), intent(in) :: excludeids
-  integer, intent(out) :: result
+  character(300), intent(in) :: fnhd, fnld, fnout
+  integer, intent(in), dimension(nhd) :: hdid
+  integer, intent(in), dimension(nld) :: ldid
+  integer, intent(in), dimension(hdcols) :: hdpos
+  integer, intent(in), dimension(ldcols) :: ldpos
+  integer, intent(out) :: status
 
-  integer :: i, j, id, stat, k
-  integer, dimension(files) :: units
-  integer, dimension(:,:), allocatable :: row
-  character(50) :: fmt0
-  character(50), dimension(files) :: fmt
-  character(4), dimension(files) :: advance
+  logical :: foundID, isint
+  integer :: stat, i, oldi, animalID
+  
+  character(50)  :: fmt
+  integer, allocatable :: intoutput(:)
+  real, allocatable :: line(:), realoutput(:)
+  ! only allocate one of these buggers.
+  
+  isint = asint == 1
+  
+  write(fmt, '(i5)') outcols
+  if (isint .eqv. .true.) then
+    fmt='(i20,'//trim(adjustl(fmt))//'I'//userfmt//')'
+    allocate(intoutput(outcols))
+    intoutput(:) = missing
+  else
+    fmt='(i20,'//trim(adjustl(fmt))//'R'//userfmt//')'
+    allocate(realoutput(outcols))
+    realoutput(:) = missing
+  end if
 
-  allocate(row(files,maxval(ncols, 1)))
+  ! Open output file.
+  open(71, file=fnout, status='UNKNOWN', iostat=stat)
+  if (stat /= 0) then
+    status = 1
+    return
+  end if
 
-  advance(:) = 'no'
-  advance(files) = 'yes'
-
-  do i=1,files
-    units(i) = 200 + i
-    open(units(i), file=fns(i), status='OLD')
-    write(fmt0, '(i5)') ncols(i)
-    fmt(i)='('//trim(adjustl(fmt0))//'I2)'
-  end do
-
-  open(55, file=fnout, status='UNKNOWN')
-
-  do j=1,nlines
-    fileloop: do i=1,files
-      read(units(i), *, iostat=stat) id, row(i,1:ncols(i))
-      if (stat /= 0) exit
-      if (j <= skiplines) cycle
-      if (idlength > 0) then
-        do k=1, idlength
-          if (excludeids(k) .eq. id) cycle fileloop
-        enddo
-      endif
-      if (i == 1) write(55, '(i20)', advance='no') id
-      write(55, fmt(i), advance=advance(i)) row(i,1:ncols(i))
-    end do fileloop
+  ! Read through HD file.
+  allocate(line(hdcols))
+  i = 0
+  oldi = 0
+  open(72, file=fnhd, status='OLD')
+  do while (.TRUE.)
+    read(72, *, iostat=stat) animalID, line
     if (stat /= 0) exit
+
+    ! Find animal in HD IDs.
+    foundID = .FALSE.
+    !oldi = i
+    !if (oldi == 0) oldi=1
+    do while (.TRUE.)
+      if (i == nhd) i = 0
+      i = i + 1
+      !print *, i, oldi, hdid(i)
+      if (hdid(i) == animalID) then
+        foundID = .TRUE.
+        exit
+      end if
+      if (i == oldi) exit
+      if (i == nhd .and. oldi == 0) exit
+    enddo
+    oldi = i
+    if (foundID) then
+      if (isint .eqv. .true.) then
+        intoutput(hdpos) = line(1:hdcols)
+        write(71, fmt) animalID, intoutput
+      else
+        realoutput(hdpos) = line(1:hdcols)
+        write(71, fmt) animalID, realoutput
+      end if
+    end if
+
   end do
+  close(72)
+  deallocate(line)
 
-  deallocate(row)
+  ! Read through LD file.
+  allocate(line(ldcols))
+  if (isint .eqv. .true.) then
+    intoutput(:) = missing
+  else
+    realoutput(:) = missing
+  endif
+  
+  i = 0
+  oldi=0
+  open(72, file=fnld, status='OLD')
+  do while (.TRUE.)
+    read(72, *, iostat=stat) animalID, line
+    if (stat /= 0) exit
 
-  close(55)
-  do i=1,files
-    close(units(i))
+
+    ! Find animal in LD IDs.
+    foundID = .FALSE.
+    oldi = i
+    if (oldi == 0) oldi=1
+    do while (.TRUE.)
+      if (i == nld) i = 0
+      i = i + 1
+      if (ldid(i) == animalID) then
+        foundID = .TRUE.
+        exit
+      end if
+      if (i == oldi) exit
+      if (i == nld .and. oldi == 0) exit
+    enddo
+    oldi = i
+    if (foundID) then
+      if (isint .eqv. .true.) then
+        intoutput(hdpos) = line(1:hdcols)
+        write(71, fmt) animalID, intoutput
+      else
+        realoutput(hdpos) = line(1:hdcols)
+        write(71, fmt) animalID, realoutput
+      end if
+    end if
+
   end do
+  close(72)
+  deallocate(line)
 
-  result=stat
+  if (isint .eqv. .true.) then
+    deallocate(intoutput)
+  else
+    deallocate(realoutput)
+  endif
 
-end subroutine cbindsnpsint
+  close(71)
+
+end subroutine combinechips
+
+
